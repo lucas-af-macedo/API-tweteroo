08073f152e29ba3ed9426142852a29e7310eacd5
Lucas Macedo <lucasamacedo@hotmail.com>
2024-01-29 17:47:33 -0300

    feat: route post tweet created

--- src/main/java/com/tweteroo/api/services/TweetService.java
+++ src/main/java/com/tweteroo/api/services/TweetService.java
@@ -4,15 +4,25 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.tweteroo.api.dtos.TweetDTO;
 import com.tweteroo.api.models.TweetModel;
 import com.tweteroo.api.repository.TweetRepository;
 
 @Service
 public class TweetService {
     final TweetRepository tweetRepository;
+    final UserService userService;
 
-    TweetService(TweetRepository tweetRepository) {
+    TweetService(TweetRepository tweetRepository, UserService userService) {
         this.tweetRepository = tweetRepository;
+        this.userService = userService;
+    }
+
+
+    public TweetModel save(TweetDTO dto){
+        if (userService.findUserById(dto.getUserId())){
+            
+        }
     }
 
     public TweetModel createTweet(TweetModel tweet){
